7e5df97c486c6908505028317832e29eecb73133
Flávio Rocha <flavio-10_28@hotmail.com>
2022-04-28 12:56:34 -0300

    Criação permissões do administrador, cadastro das especialidades

---
+++ demo-security/src/main/java/com/mballem/curso/security/repository/EspecialidadeRepository.java
@@ -0,0 +1,17 @@
+package com.mballem.curso.security.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.mballem.curso.security.domain.Especialidade;
+
+@Repository
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
+
+	@Query("SELECT e FROM Especialidade e WHERE e.titulo LIKE :search%")
+	public Page<Especialidade> findAllByTitulo(String search, Pageable pageable);
+
+}
