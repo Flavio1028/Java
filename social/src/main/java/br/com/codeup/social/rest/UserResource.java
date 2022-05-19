361ca74d398389569d24ccd932828df4dbd22bcb
Flávio Rocha <flavio-10_28@hotmail.com>
2022-05-19 17:33:40 -0300

    Criação rota USERS

---
+++ social/src/main/java/br/com/codeup/social/rest/UserResource.java
@@ -0,0 +1,18 @@
+package br.com.codeup.social.rest;
+
+import br.com.codeup.social.rest.dto.CreateUserRequest;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+public class UserResource {
+
+    @POST
+    public Response createUser(CreateUserRequest userRequest) {
+
+        return Response.ok().build();
+    }
+
+}
