f360d177f309207aab3edef6b35f13cd831c6e0a
Flávio Rocha <flavio-10_28@hotmail.com>
2022-05-20 12:35:43 -0300

    Criação da entidade e do insert

--- social/src/main/java/br/com/codeup/social/rest/UserResource.java
+++ social/src/main/java/br/com/codeup/social/rest/UserResource.java
@@ -1,16 +1,34 @@
 package br.com.codeup.social.rest;
 
+import br.com.codeup.social.domain.model.User;
 import br.com.codeup.social.rest.dto.CreateUserRequest;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
+    @Transactional
     public Response createUser(CreateUserRequest userRequest) {
+
+        User user = new User();
+
+        user.setName(userRequest.getName());
+        user.setAge(userRequest.getAge());
+
+        user.persist();
+
+        return Response.ok(user).build();
+    }
+
+    @GET
+    public Response listAllUsers() {
 
         return Response.ok().build();
     }
