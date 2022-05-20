53612aee7df7c1c7d4476da08b64f8b372936f06
Flávio Rocha <flavio-10_28@hotmail.com>
2022-05-20 14:56:19 -0300

    Validando entrada de dados

--- social/src/main/java/br/com/codeup/social/rest/UserResource.java
+++ social/src/main/java/br/com/codeup/social/rest/UserResource.java
@@ -3,12 +3,16 @@ package br.com.codeup.social.rest;
 import br.com.codeup.social.domain.model.User;
 import br.com.codeup.social.domain.repository.UserRepository;
 import br.com.codeup.social.rest.dto.CreateUserRequest;
+import br.com.codeup.social.rest.dto.ResponseError;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Set;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -16,24 +20,29 @@ import javax.ws.rs.core.Response;
 public class UserResource {
 
     private UserRepository repository;
+    private Validator validator;
 
     @Inject
-    public UserResource(UserRepository repository) {
+    public UserResource(UserRepository repository, Validator validator) {
         this.repository = repository;
+        this.validator = validator;
     }
 
     @POST
     @Transactional
     public Response createUser(CreateUserRequest userRequest) {
 
-        User user = new User();
+        Set<ConstraintViolation<CreateUserRequest>> violations = this.validator.validate(userRequest);
+        if(!violations.isEmpty()) {
+            return Response.status(422).entity(ResponseError.createFormValidation(violations)).build();
+        }
 
+        User user = new User();
         user.setName(userRequest.getName());
         user.setAge(userRequest.getAge());
-
         user.persist();
 
-        return Response.ok(user).build();
+        return Response.status(Response.Status.CREATED).entity(user).build();
     }
 
     @GET
@@ -50,7 +59,7 @@ public class UserResource {
 
         if(user != null) {
             user.delete();
-            return Response.ok().build();
+            return Response.status(Response.Status.NO_CONTENT).build();
         }
 
         return Response.status(Response.Status.NOT_FOUND).build();
@@ -60,15 +69,19 @@ public class UserResource {
     @Transactional
     public Response updateUser(@PathParam("id") Long userId, CreateUserRequest userData) {
 
-        User user = User.findById(userId);
+        Set<ConstraintViolation<CreateUserRequest>> violations = this.validator.validate(userData);
+        if(!violations.isEmpty()) {
+            return Response.status(422).entity(ResponseError.createFormValidation(violations)).build();
+        }
 
+        User user = User.findById(userId);
         if(user != null) {
             user.setName(userData.getName());
             user.setAge(userData.getAge());
 
             user.persist();
 
-            return Response.ok().build();
+            return Response.status(Response.Status.NO_CONTENT).build();
         }
 
         return Response.status(Response.Status.NOT_FOUND).build();
---
+++ social/src/main/java/br/com/codeup/social/rest/dto/ResponseError.java
@@ -0,0 +1,43 @@
+package br.com.codeup.social.rest.dto;
+import javax.validation.ConstraintViolation;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ResponseError {
+
+    private String message;
+    private Collection<FieldError> erros;
+
+    public ResponseError(String message, Collection<FieldError> erros) {
+        this.message = message;
+        this.erros = erros;
+    }
+
+    public static <T> ResponseError createFormValidation(Set<ConstraintViolation<T>> violations) {
+        List<FieldError> error = violations
+                .stream()
+                .map(cv -> new FieldError(cv.getPropertyPath().toString(), cv.getMessage()))
+                .collect(Collectors.toList());
+
+        String message = "Validation Error";
+        return new ResponseError(message, error);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Collection<FieldError> getErros() {
+        return erros;
+    }
+
+    public void setErros(Collection<FieldError> erros) {
+        this.erros = erros;
+    }
+}
