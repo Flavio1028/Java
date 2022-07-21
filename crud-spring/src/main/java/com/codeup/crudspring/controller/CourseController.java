51306a8598ae85b5a2b8fa8c0bc1cdcc567f8a56
Flávio Rocha <flavio-10_28@hotmail.com>
2022-07-21 16:00:59 -0300

    Criação salvar curso

--- crud-spring/src/main/java/com/codeup/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/codeup/crudspring/controller/CourseController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import com.codeup.crudspring.model.Course;
 import com.codeup.crudspring.repository.CourseRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +27,11 @@ public class CourseController {
     public @ResponseBody List<Course> list() {
         return repository.findAll();
     }
+
+	@PostMapping
+	public ResponseEntity<Course> create(@RequestBody Course course) {
+		course = repository.save(course);
+		return ResponseEntity.status(HttpStatus.CREATED).body(course);
+	}
 
 }
