4705dfc08459c629827e3457a65ceae12a7765be
Flávio Rocha <flavio-10_28@hotmail.com>
2022-11-19 11:56:48 -0300

    Criação do atualizar curso

--- crud-spring/src/main/java/com/codeup/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/codeup/crudspring/controller/CourseController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,7 +32,7 @@ public class CourseController {
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Course> findById(@PathVariable("id") Long id) {
-		return repository.findById(id).map(record -> ResponseEntity.ok().body(record))
+		return repository.findById(id).map(recordFound -> ResponseEntity.ok().body(recordFound))
 				.orElse(ResponseEntity.notFound().build());
 	}
 
@@ -39,6 +40,17 @@ public class CourseController {
 	public ResponseEntity<Course> create(@RequestBody Course course) {
 		course = repository.save(course);
 		return ResponseEntity.status(HttpStatus.CREATED).body(course);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Course> update(@PathVariable("id") Long id, 
+	        @RequestBody Course course) {
+	    return repository.findById(id).map(recordFound -> {
+	        recordFound.setName(course.getName());
+	        recordFound.setCategory(course.getCategory());
+	        Course updated = repository.save(recordFound);
+	        return ResponseEntity.ok().body(updated);
+	    }).orElse(ResponseEntity.notFound().build());
 	}
 
 }
